2ee2c9af1382a362f595470f2f0df3761f8caa8e
analog-nico <nicolai.kamenzky@testrails.org>
2014-10-21 17:42:43 -0400

    Set up tooling

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+var chalk = require('chalk');
+var rimraf = require('rimraf');
+
+
+var paths = {
+    libFiles: './lib/**/*.js',
+    gulpfile: './gulpfile.js',
+    specFiles: './test/spec/**/*.js',
+    fixtureFiles: './test/fixtures/**/*.js'
+};
+
+
+gulp.task('dev', ['watch', 'validate']);
+
+gulp.task('watch', function () {
+
+    return gulp.watch([
+        paths.libFiles,
+        paths.gulpfile,
+        paths.specFiles,
+        paths.fixtureFiles
+    ], [
+        'validate'
+    ]);
+
+});
+
+gulp.task('validate', function (done) {
+    runSequence('lint', 'test', done);
+});
+
+gulp.task('lint', function () {
+
+    return gulp.src([paths.libFiles, paths.gulpfile, paths.specFiles, paths.fixtureFiles])
+        .pipe(jshint())
+        .pipe(jshint.reporter(jshintStylish))
+        .pipe(jshint.reporter('fail'));
+
+});
+
+gulp.task('test', ['clean'], function (done) {
+
+    var coverageVariable = '$$cov_' + new Date().getTime() + '$$';
+
+    gulp.src(paths.libFiles)
+        .pipe(istanbul({
+            coverageVariable: coverageVariable
+        }))
+        .on('finish', function () {
+
+            gulp.src(paths.specFiles)
+                .pipe(mocha())
+                .on('error', function () {
+                    console.log(chalk.bold.bgRed(' TESTS FAILED '));
+                    done();
+                })
+                .pipe(istanbul.writeReports({
+                    reporters: ['lcov'],
+                    coverageVariable: coverageVariable
+                }))
+                .on('end', done);
+
+        });
+
+});
+
+gulp.task('test-without-coverage', function () {
+
+    return gulp.src(paths.specFiles)
+        .pipe(mocha())
+        .on('error', function () {
+            console.log(chalk.bold.bgRed(' TESTS FAILED '));
+        });
+
+});
+
+gulp.task('clean', function (done) {
+    rimraf('./coverage', done);
+});
