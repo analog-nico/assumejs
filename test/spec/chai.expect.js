0fce2104924b788ec4a5255c0fdd3447cbddfd7a
analog-nico <nicolai.kamenzky@testrails.org>
2014-10-22 20:56:16 -0400

    Added test

--- test/spec/chai.expect.js
+++ test/spec/chai.expect.js
@@ -5,6 +5,12 @@ var assume = require('../../lib/assume.js');
 
 describe('Assume.js embodying chai.expect', function () {
 
+    it('should not have altered expect() used in these tests - just to be sure', function () {
+
+        expect(function () { expect(true).to.eql(false); }).to.throw();
+
+    });
+
     it('should allow basic assertions', function () {
 
         expect(function () { assume(true).to.eql(true);  }).to.not.throw();
